7c32db8491dfc29e7266784a748031d1e4d70414
MatteoFilosa <filosa.1793997@studenti.uniroma1.it>
2023-05-01 17:28:43 +0200

    Aggregato visualizzazione singola e attributi sistemato (manca normalizzazione)

--- material/test_api_data.py
+++
@@ -1,6 +0,0 @@
-import requests
-import pandas as pd
-
-x = requests.get('http://arca.diag.uniroma1.it:8082/rodaggi/spindles/1/raw_data?limit=100')
-df = pd.DataFrame(x.json()['data'])
-print(df.head())
